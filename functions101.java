e7fbbd28ed2a1b38642e2545ec8364f11cf315ce
Ubuntu <ubuntu@ip-10-160-130-242.us-west-1.compute.internal>
2014-02-05 20:09:58 +0000

    stub class

---
+++ functions101.java
@@ -0,0 +1,10 @@
+public abstract class Functions101{
+
+public void test1(int test1);
+public void test2(int test1);
+public void test3(int test1);
+public void test4(int test1);
+public void test5(int test1);
+public void test6(int test1);
+
+}
